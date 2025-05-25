f67c5a500800afcb63158db2fd359ecca7fce724
Atifboii <syedatifarsalan@gmail.com>
2025-05-25 20:07:12 +0600

    Intro by Arsalan :D

---
+++ Intro/script.js
@@ -0,0 +1,15 @@
+let student = 3
+document.writeln(`I have ${student} students.`);
+document.getElementById("p1").innerHTML = `I have ${student} students`;
+let num = 0;
+const num2 = 3.14159;
+let text = "fdjgufds";
+document.writeln(`<br>`);
+document.writeln(`The data typr of ${num} is a ${typeof num}`);
+document.writeln(`<br>`);
+document.writeln(`The data typr of ${num2} is a ${typeof num2}`);
+document.writeln(`<br>`);
+document.writeln(`The data typr of ${text} is a ${typeof text}`);
+function clicking() {
+    document.getElementById("p2").innerHTML = ' I have been clicked '
+}
