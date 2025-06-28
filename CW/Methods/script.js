8136a3404cefd95cd8e4c414ebc6a5e0c56fa81a
Atifboii <syedatifarsalan@gmail.com>
2025-06-28 20:39:23 +0600

    lOOPS

--- CW/Methods/script.js
+++ CW/Methods/script.js
@@ -5,3 +5,10 @@ document.getElementById("p3").innerHTML = `${Word.toLowerCase(Word)}`
 let Phr = new RegExp ("Hello!")
 let Phr2 = Phr.test(Word)
 document.getElementById("p4").innerHTML = Phr2
+let fruits = ["Apple","Mango","Banana","Grapes","Watermelon"]
+let text = ""
+let i
+for (i=0; i<fruits.length; i++){
+    text += fruits[i]+ "<br/>"
+}
+document.getElementById("p5").innerHTML = text
